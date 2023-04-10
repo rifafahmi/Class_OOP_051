2c98b7af81e6d22097dacc0fb682d738512e2495
Rifa <rifafahim21.com@gmial.com>
2023-04-10 11:04:52 +0700

    sudah jadi lupa commit

--- Class_OOP_051/Class_OOP_051.cpp
+++ Class_OOP_051/Class_OOP_051.cpp
@@ -7,7 +7,47 @@ public:
 	string nama;
 public: void	cetak() {
 	cout << "NIM = " << nim;
-	cout << 
+	cout << "\nNama = " << nama;
 }
 };
 
+class matakuliah {
+private:
+	string kode;
+	string namaM;
+	int sks;
+public:
+	void input() {
+		cout << " Masukkan Kode Matakuliah :";
+		cin >> kode;
+		cout << "Masukkan Nama Matakuliah :";
+		cin >> namaM;
+		cout << " Jumlah SKS";
+		cin >> sks;
+
+	}
+	void tampil() {
+		cout << " Kode matakuliah : ";
+		cin >> kode;
+		cout << "Nama Matakuliah : ";
+		cin >> namaM;
+		cout << " Jumlah SKS:";
+		cin >> sks;
+	}
+};
+
+int main ()
+{
+	mahasiswa mhs1;
+	matakuliah mk;
+
+	cout << "Masukan NIM ";
+	cin >> mhs1.nim;
+	cout << " Masukkan Nama :";
+	cin >> mhs1.nama;
+	mhs1.cetak();
+
+	mk.input();
+	mk.tampil();
+
+}
